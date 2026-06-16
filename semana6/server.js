debad43c71f8107e6902bc687b33ed9b7b5ce019
Natalia Martinez <nataliamartinezsuarez33@gmail.com>
2026-06-16 14:00:33 -0500

    subiendo tarea semana6

---
+++ semana6/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const app = express();
+
+
+// Permite recibir datos JSON desde peticiones POST
+app.use(express.json());
+
+
+// Ruta de registro
+app.post('/registro', (req, res) => {
+
+    const nombre = req.body.nombre;
+    const mensaje = req.body.mensaje;
+
+    res.json({
+        estado: "Datos recibidos",
+        nombre: nombre,
+        mensaje: mensaje
+    });
+
+});
+
+
+// Ruta para registrar incidencias
+app.post('/incidencia', (req, res) => {
+
+    const tipo = req.body.tipo;
+    const descripcion = req.body.descripcion;
+
+    res.json({
+        mensaje: "Incidencia registrada",
+        tipo: tipo,
+        descripcion: descripcion
+    });
+
+});
+
+
+// Encender servidor
+app.listen(3000, () => {
+
+    console.log("Servidor ejecutándose en puerto 3000");
+
+});
